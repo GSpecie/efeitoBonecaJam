e5d161c71774da322f8d59fa608fde9a1571fbe7
Gabriel Adao <gabriel.adao@outlook.com.br>
2022-09-07 00:33:53 -0300

    enemy follow
    enemy follow, need to change in code to follow only player

--- Efeito Boneca/Assets/Scripts/Enemy/Enemy.cs
+++ Efeito Boneca/Assets/Scripts/Enemy/Enemy.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float actualHealth, maxHealth = 3f;
+    Rigidbody rb;
+    [SerializeField] Transform target;
+    public float moveSpeed = 4f;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         actualHealth = maxHealth;
     }
 
+    private void FixedUpdate()
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(pos);
+        transform.LookAt(target);
+    }
     public void TakeDamage(float damageAmount)
     {
         actualHealth -= damageAmount;
